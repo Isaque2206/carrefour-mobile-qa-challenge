6095ae09919fda8bf94f10a26b690e94c0fc8f0a
Isaque Silva <isaque.antunes@hotmail.com>
2025-09-01 13:02:20 -0300

    ci: fix apk download + gh-pages permissions; wdio usa APK_PATH

--- wdio.android.local.conf.js
+++ wdio.android.local.conf.js
@@ -1,30 +1,49 @@
-
 const path = require('path');
-const appPath = path.resolve(__dirname, 'apps', 'native-demo-app-android.apk'); 
+const appPath = path.resolve(__dirname, 'apps', 'native-demo-app-android.apk');
 
 exports.config = {
-  // ...
+  runner: 'local',
+
   specs: ['./test/specs/**/*.spec.js'],
 
+  maxInstances: 1,
+
+  
   services: [
     ['appium', {
       args: {
         address: '127.0.0.1',
         port: 4723,
-        basePath: '/'
+        basePath: '/'     
       }
     }]
   ],
 
- capabilities: [{
-  platformName: 'Android',
-  'appium:automationName': 'UiAutomator2',
-  'appium:deviceName': 'Android Emulator',
-  'appium:platformVersion': '13',
-  'appium:noReset': true,
-  'appium:appPackage': 'com.wdiodemoapp',
-  'appium:appActivity': 'com.wdiodemoapp.MainActivity',
-}],
+  framework: 'mocha',
+  reporters: [
+    'spec',
+    ['allure', { outputDir: 'allure-results', disableWebdriverStepsReporting: true }]
+  ],
 
-  maxInstances: 1
+  capabilities: [{
+    platformName: 'Android',
+    'appium:automationName': 'UiAutomator2',
+    'appium:deviceName': 'Android Emulator',
+   
+    'appium:platformVersion': '13',
+
+   
+    'appium:app': process.env.APK_PATH || appPath,
+
+  
+
+    'appium:autoGrantPermissions': true,
+    'appium:newCommandTimeout': 240,
+    'appium:noReset': true
+  }],
+
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 600000
+  }
 };
