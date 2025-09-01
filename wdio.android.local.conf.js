4414bc187f3c7919be51a5061b389112dbe5e7d4
Isaque Silva <isaque.antunes@hotmail.com>
2025-09-01 13:16:29 -0300

    ci: fix pipeline (deps, apk, emulator)

--- wdio.android.local.conf.js
+++ wdio.android.local.conf.js
@@ -3,11 +3,10 @@ const appPath = path.resolve(__dirname, 'apps', 'native-demo-app-android.apk');
 
 exports.config = {
   runner: 'local',
-
   specs: ['./test/specs/**/*.spec.js'],
   maxInstances: 1,
 
- 
+  // Conecta no Appium iniciado pelo workflow
   hostname: '127.0.0.1',
   port: 4723,
   path: '/',
@@ -18,6 +17,7 @@ exports.config = {
     ['allure', { outputDir: 'allure-results', disableWebdriverStepsReporting: true }]
   ],
 
+  // Appium é iniciado no YAML, então não usamos o service aqui
   services: [],
 
   capabilities: [{
@@ -25,22 +25,16 @@ exports.config = {
     'appium:automationName': 'UiAutomator2',
     'appium:deviceName': 'Android Emulator',
     'appium:platformVersion': '13',
-
-    
+    // Instala o app a partir do APK baixado na pipeline
     'appium:app': process.env.APK_PATH || appPath,
-
-   
+    // Não misture com appPackage/appActivity quando usar "app"
     'appium:autoGrantPermissions': true,
     'appium:newCommandTimeout': 240,
     'appium:noReset': false,
     'appium:adbExecTimeout': 120000
   }],
 
-  mochaOpts: {
-    ui: 'bdd',
-    timeout: 600000
-  },
-
+  mochaOpts: { ui: 'bdd', timeout: 600000 },
   waitforTimeout: 20000,
   connectionRetryTimeout: 120000,
   connectionRetryCount: 3
