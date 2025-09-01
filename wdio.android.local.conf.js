b28726a1d593563bc7c5eafa4c3203b89c0b1dab
Isaque Silva <isaque.antunes@hotmail.com>
2025-09-01 15:03:19 -0300

    test: alinhar WDIO com emulador (Android 11) e limitar a 1 worker

--- wdio.android.local.conf.js
+++ wdio.android.local.conf.js
@@ -4,38 +4,38 @@ const appPath = path.resolve(__dirname, 'apps', 'native-demo-app-android.apk');
 exports.config = {
   runner: 'local',
   specs: ['./test/specs/**/*.spec.js'],
-  maxInstances: 1,
 
-  // Conecta no Appium iniciado pelo workflow
-  hostname: '127.0.0.1',
-  port: 4723,
-  path: '/',
-
+  
   framework: 'mocha',
   reporters: [
     'spec',
     ['allure', { outputDir: 'allure-results', disableWebdriverStepsReporting: true }]
   ],
+  mochaOpts: { ui: 'bdd', timeout: 600000 },
 
-  // Appium é iniciado no YAML, então não usamos o service aqui
-  services: [],
+  
+  hostname: '127.0.0.1',
+  port: 4723,
+  path: '/',
 
+  
   capabilities: [{
+    maxInstances: 1,                       // << só 1 worker de teste
     platformName: 'Android',
     'appium:automationName': 'UiAutomator2',
     'appium:deviceName': 'Android Emulator',
-    'appium:platformVersion': '13',
-    // Instala o app a partir do APK baixado na pipeline
+    'appium:udid': 'emulator-5554',       
     'appium:app': process.env.APK_PATH || appPath,
-    // Não misture com appPackage/appActivity quando usar "app"
     'appium:autoGrantPermissions': true,
     'appium:newCommandTimeout': 240,
     'appium:noReset': false,
     'appium:adbExecTimeout': 120000
   }],
 
-  mochaOpts: { ui: 'bdd', timeout: 600000 },
+ 
   waitforTimeout: 20000,
   connectionRetryTimeout: 120000,
-  connectionRetryCount: 3
+  connectionRetryCount: 3,
+
+  services: [] 
 };
